303a1799b67ac7a947f16923ce21fb80c69c9158
Antonin Januska <antonin@antjanus.com>
2015-10-13 00:48:26 -0600

    Showdown

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,10 @@
 /// <reference path="../typings/angular2/angular2.d.ts" />
+/// <reference path="../typings/showdown/showdown.d.ts" />
 import 'zone.js';
 import 'reflect-metadata';
 import 'es6-shim';
+
+import * as showdown from 'showdown';
 
 import {Component, View, bootstrap} from 'angular2/angular2';
 
@@ -12,8 +15,14 @@ import {Component, View, bootstrap} from 'angular2/angular2';
   templateUrl: '/app/markdownApp.html'
 })
 class MarkdownAppComponent {
+  public html: string;
 
   constructor() {
+    this.html = '';
+  }
+
+  public updateValue(val) {
+    this.html = val;
   }
 }
 
