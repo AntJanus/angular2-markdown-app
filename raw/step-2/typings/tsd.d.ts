1c17162e8dc7ed0205ea028991b15740339574a4
Antonin Januska <antonin@antjanus.com>
2015-12-11 14:02:27 -0700

    Step 2 brought to the future of Alpha-52

--- raw/step-2/typings/tsd.d.ts
+++ raw/step-2/typings/tsd.d.ts
@@ -1,5 +1,1 @@
-/// <reference path="angular2/angular2.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
-/// <reference path="rx/rx-lite.d.ts" />
-/// <reference path="rx/rx.d.ts" />
 /// <reference path="marked/marked.d.ts" />
